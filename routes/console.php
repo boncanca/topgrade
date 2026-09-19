156fc02bf5ddb32d473e9b9a4fbccc0701ef9e7a
tomc <tomc@leanicontechnology.com>
2026-09-19 14:41:58 +0000

    chore(scheduler): schedule bookings:expire-pending every 5 minutes

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Expire pending booking reservations that have exceeded their 30-minute window
+Schedule::command('bookings:expire-pending')
+    ->everyFiveMinutes()
+    ->withoutOverlapping();
